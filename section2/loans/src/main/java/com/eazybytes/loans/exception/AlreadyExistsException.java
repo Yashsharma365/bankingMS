3cfc2d5df7ee564209e7788fd04876dca61c6209
Yash sharma <60930724+Yashsharma365@users.noreply.github.com>
2025-06-29 14:58:37 +0530

    Chnages

--- section2/loans/src/main/java/com/eazybytes/loans/exception/AlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.eazybytes.loans.exception;
-
-public class AlreadyExistsException extends RuntimeException{
-
-    public AlreadyExistsException(String message){
-        super(message);
-    }
-}
--- section2/loans/src/main/java/com/eazybytes/loans/exception/ResourceNotAvailableException.java
+++
@@ -1,8 +0,0 @@
-package com.eazybytes.loans.exception;
-
-public class ResourceNotAvailableException extends RuntimeException{
-
-    public ResourceNotAvailableException(String resourceName,String fieldName,String fieldValue){
-        super(String.format("%s Not found with %s : %s",resourceName,fieldName,fieldValue));
-    }
-}

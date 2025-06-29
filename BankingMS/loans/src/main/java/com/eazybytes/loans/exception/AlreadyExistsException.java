39f54e18b34a3b7a624b6131bb9f01dc442a7a15
Yash sharma <60930724+Yashsharma365@users.noreply.github.com>
2025-06-29 17:32:31 +0530

    Config changes - rabbit added,actuator changes

---
+++ BankingMS/loans/src/main/java/com/eazybytes/loans/exception/AlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.eazybytes.loans.exception;
+
+public class AlreadyExistsException extends RuntimeException{
+
+    public AlreadyExistsException(String message){
+        super(message);
+    }
+}
